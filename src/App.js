b312224f6723f2d6cc75a1876b85c975be25a337
[Patrick Bradshaw] <[patrick.j.bradshaw@gmail.com]>
2019-07-14 21:05:31 -0600

    Updating app w/ shop page, collection preview comp., collection item comp.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
 import './App.css';
 
-import HomePage from './pages/homepage/homepage.component'
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
 function App () {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
+      </Switch>
     </div>
   );
 }
