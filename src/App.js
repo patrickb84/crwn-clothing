185061aeb30479a09cbe77deecc261396a15d79d
Patrick Bradshaw <pbradshaw@advantage.local>
2019-07-15 14:13:03 -0600

    1:31 into lesson 87...

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
 import Header from './components/header/header.component';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 class App extends React.Component {
   constructor() {
@@ -20,19 +20,19 @@ class App extends React.Component {
 
   unsubscribeFromAuth = null
 
-  componentDidMount () {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
-
-      console.log(user);
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async user => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+      }
     });
   }
 
-  componentWillUnmount () {
+  componentWillUnmount() {
     this.unsubscribeFromAuth();
   }
 
-  render () {
+  render() {
     return (
       <div>
         <Header currentUser={this.state.currentUser} />
