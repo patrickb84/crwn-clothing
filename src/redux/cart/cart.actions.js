8ba7e987e4a443ea51021bda81b816a6084ad45a
[Patrick Bradshaw] <[patrick.j.bradshaw@gmail.com]>
2019-07-16 23:42:54 -0600

    updated shop data w/ new data, updated cart utils to group items in cart reducer

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -1,5 +1,10 @@
 import CartActionTypes from './cart.types';
 
 export const toggleCartHidden = () => ({
-  type: CartActionTypes.TOGGLE_CART_HIDDEN
+	type: CartActionTypes.TOGGLE_CART_HIDDEN
+});
+
+export const addItem = item => ({
+	type: CartActionTypes.ADD_ITEM,
+	payload: item
 });
--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,19 +1,26 @@
-import CartActionTypes from './cart.types'
+import CartActionTypes from './cart.types';
+import { addItemToCart } from './cart.utils';
 
 const INITIAL_STATE = {
-  hidden: true
+	hidden: true,
+	cartItems: []
 };
 
 const cartReducer = (state = INITIAL_STATE, action) => {
-  switch (action.type) {
-    case CartActionTypes.TOGGLE_CART_HIDDEN:
-      return { 
-        ...state,
-        hidden: !state.hidden
-      }
-      default:
-        return state;
-  }
-}
+	switch (action.type) {
+		case CartActionTypes.TOGGLE_CART_HIDDEN:
+			return {
+				...state,
+				hidden: !state.hidden
+			};
+		case CartActionTypes.ADD_ITEM:
+			return {
+				...state,
+				cartItems: addItemToCart(state.cartItems, action.payload)
+			};
+		default:
+			return state;
+	}
+};
 
 export default cartReducer;
